48e3775d2ecfe509bf6dd9090752549421b18f3f
Craig Wellspring <wellspring.craig@gmail.com>
2021-09-14 18:33:35 -0500

    item API calls

---
+++ src/scripts/helpers/data/item-data.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import firebaseConfig from '../../../api/apiKeys';
+
+const dbURL = firebaseConfig.databaseURL;
+
+// Get all items
+const getAllItems = () => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/items.json`)
+    .then((response) => resolve(Object.values(response)))
+    .catch(reject);
+});
+
+// Get single item
+const getItem = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/items/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+// Get all items on specified Order
+const getOrderItems = (orderKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbURL}/items.json?orderBy="orderKey"&equalTo="${orderKey}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+// Create item
+const createItem = (itemObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbURL}/items.json`, itemObj)
+    .then((response) => {
+      const patchPayload = { firebaseKey: response.data.name };
+      axios.patch(`${dbURL}/items/${response.data.name}.json`, patchPayload)
+        .then(() => {
+          getAllItems().then(resolve);
+        });
+    }).catch(reject);
+});
+
+// Delete vocabulary entry
+const deleteItem = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbURL}/vocab/${firebaseKey}.json`)
+    .then(() => {
+      getAllItems().then(resolve);
+    }).catch(reject);
+});
+
+// Update vocabulary entry
+const updateItem = (firebaseKey, payload) => new Promise((resolve, reject) => {
+  axios.patch(`${dbURL}/vocab/${firebaseKey}.json`, payload)
+    .then(() => getAllItems().then(resolve))
+    .catch(reject);
+});
+
+export {
+  getAllItems,
+  getItem,
+  getOrderItems,
+  createItem,
+  deleteItem,
+  updateItem
+};
