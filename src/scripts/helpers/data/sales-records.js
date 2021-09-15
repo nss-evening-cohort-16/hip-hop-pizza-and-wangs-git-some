3c579fec18bf788be7a74de4da6e69ce58b15dca
Craig Wellspring <wellspring.craig@gmail.com>
2021-09-14 19:35:29 -0500

    records helpers

---
+++ src/scripts/helpers/data/sales-records.js
@@ -0,0 +1,35 @@
+import { getAllSalesRecords } from './sales-data';
+
+// Get Total Sales and Tips from all records in a given array
+const getTotals = (recordArray) => {
+  let revenue = 0;
+  let tips = 0;
+  recordArray.forEach((record) => {
+    revenue += record.order_total;
+    tips += record.tip;
+  });
+
+  return `${revenue}--${tips}`;
+};
+
+// Get records from a certain date
+const getRecordsFromDate = (date) => {
+  getAllSalesRecords().then((records) => records.filter((record) => record.date === date));
+};
+
+// Get all sales records of a certain payment type
+const getRecordsByPaymentType = (paymentType) => {
+  getAllSalesRecords().then((records) => records.filter((record) => record.paymentType === paymentType));
+};
+
+// Get all sales records of a certain order type
+const getRecordsByOrderType = (orderType) => {
+  getAllSalesRecords().then((records) => records.filter((record) => record.orderType === orderType));
+};
+
+export {
+  getTotals,
+  getRecordsFromDate,
+  getRecordsByPaymentType,
+  getRecordsByOrderType
+};
