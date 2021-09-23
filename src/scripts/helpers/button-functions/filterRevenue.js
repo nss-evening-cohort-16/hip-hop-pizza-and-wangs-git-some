12403d52a7b9114ca1be9e8cf22186d36210e645
Craig Wellspring <wellspring.craig@gmail.com>
2021-09-22 21:53:24 -0500

    admin fix

--- src/scripts/helpers/button-functions/filterRevenue.js
+++ src/scripts/helpers/button-functions/filterRevenue.js
@@ -1,11 +1,11 @@
 import { generateSalesGraph, getDatesArray, getRecordsByDateRange } from '../../components/revenue';
 
-const filterRevenue = () => {
+const filterRevenue = (uid, isAdmin) => {
   const date1 = document.querySelector('#dateSelect1').value;
   const date2 = document.querySelector('#dateSelect2').value;
   const dateRange = getDatesArray(date1, date2);
 
-  getRecordsByDateRange(dateRange).then(generateSalesGraph);
+  getRecordsByDateRange(dateRange, uid, isAdmin).then(generateSalesGraph);
 };
 
 export default filterRevenue;
